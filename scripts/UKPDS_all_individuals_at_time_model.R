03083f11b67de55eb6437c8894657dde3dbb8b14
Wael Mohammed <wmamohammed1@sheffield.ac.uk>
2025-03-17 11:47:18 +0000

    cosmetic changes

--- scripts/UKPDS_all_individuals_at_time_model.R
+++ scripts/UKPDS_all_individuals_at_time_model.R
@@ -110,27 +110,35 @@ print(dimnames(a_ind_traits)) # to verify the dimension names
 
 # Step 3: Define functions for risk factor progression ####
 # Function for linear progression of risk factors
+
 #' Calculate Biomarkers 
 #'
-#' This function calculates patient-specific factors to predict the time path of a biomarker. 
+#' This function calculates patient-specific factors to predict the time path of
+#' a biomarker. 
 #'
 #' @param a_ind_traits An array containing patient characteristics over time.
-#' @param a_coef_ukpds_ind_traits A 3D array of coefficients used for calculating risk.
-#' @param biomarker_eq A character string specifying the health outcome equation (e.g., "ihd").
-#' @param time_step An integer indicating the row in `m_ind_traits` to use for calculations.
+#' @param a_coef_ukpds_ind_traits A 3D array of coefficients used for
+#' calculating risk.
+#' @param biomarker_eq A character string specifying the health outcome equation
+#' (e.g., "ihd").
+#' @param time_step An integer indicating the row in `m_ind_traits` to use for
+#' calculations.
 #' 
 #' @return The updated biomarker is stored.
 #' @export
-biomarker <- function(a_ind_traits, a_coef_ukpds_ind_traits, biomarker_eq,  time_step) {
+biomarker <- function(
+    a_ind_traits, 
+    a_coef_ukpds_ind_traits, 
+    biomarker_eq,  
+    time_step) {
   
   # Calculate patient-specific factors using model coefficients and patient data
-  updated_biomarker <- (a_ind_traits[,,time_step] %*%  a_coef_ukpds_ind_traits[,  biomarker_eq, 1] + 
-                           a_coef_ukpds_other_ind_traits["lambda",  biomarker_eq, 1] )
+  updated_biomarker <- a_ind_traits[,,time_step] %*%
+    a_coef_ukpds_ind_traits[,  biomarker_eq, 1] +
+    a_coef_ukpds_other_ind_traits["lambda",  biomarker_eq, 1]
   
   return(updated_biomarker)
 }
-
-
 
 # Step 4: Create a function to apply all risk factor models ####
 # Combine risk factor functions into a single pipeline
