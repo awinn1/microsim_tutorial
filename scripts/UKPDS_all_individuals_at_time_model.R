f374856ca26c9cc260e8f650bf5e14d254da6f0e
Wael Mohammed <wmamohammed1@sheffield.ac.uk>
2025-03-17 15:44:13 +0000

    updated gompertz_event function

--- scripts/UKPDS_all_individuals_at_time_model.R
+++ scripts/UKPDS_all_individuals_at_time_model.R
@@ -584,44 +584,84 @@ update_health_events <- function(
 # Step 7: Define a mortality function ####
 # Combine relevant event functions affecting mortality
 # Estimate survival probability over time
-#' Calculate Transition Probability Based on a Gompertz Model and Update Patient State
+#' @title Calculate Transition Probability Based on a Gompertz Model and Update
+#' Patient State
 #'
+#' @details
+#' This function calculates patient-specific factors, cumulative hazards,
+#' and the transition probability for mortality using a Gompertz model.
+#' The function determines whether the mortality event occurs at the specified
+#' time step.
 #'
-#' This function calculates patient-specific factors, cumulative hazards, 
-#' and the transition probability for mortality. 
-#' The function updates the provided `m_ind_traits` matrix with the event occurrence 
-#' at the specified time step.
+#' @param m_ind_traits_step A matrix containing patient characteristics at a
+#' specific time step (obtained from `a_ind_traits[,,time_step]`).
+#' @param a_coef_ukpds_ind_traits A 3D array of coefficients used for
+#' calculating risk.
+#' @param a_coef_ukpds_other_ind_traits A 3D array of other coefficients used
+#' for calculating risk (e.g., rho for Gompertz).
+#' @param health_outcome A character string specifying the health outcome
+#' equation (e.g., "death_yhne").
 #'
-#' @param a_ind_traits An array containing patient characteristics over time.
-#' @param a_coef_ukpds_ind_traits A 3D array of coefficients used for calculating risk.
-#' @param health_outcome A character string specifying the health outcome equation (e.g., "ihd").
-#' @param health_event A character string specifying the health outcome event in the patient trace.
-#' @param time_step An integer indicating the row in `m_ind_traits` to use for calculations.
-#' 
-#' @return The event occurrence stored.
+#' @return A logical vector indicating whether the mortality event occurred for
+#' each individual.
 #' @export
-gompertz_event <- function(a_ind_traits, a_coef_ukpds_ind_traits, health_outcome, health_event, time_step) {
-  
+#'
+#' @examples
+#' \dontrun{
+#' # Assuming a_ind_traits, a_coef_ukpds_ind_traits, and
+#' # a_coef_ukpds_other_ind_traits are already defined.
+#' time_step <- 1
+#' health_outcome <- "death_yhne"
+#'
+#' # Extract the matrix for the specific time step
+#' m_ind_traits_step <- a_ind_traits[,,time_step]
+#'
+#' # Calculate the probability of mortality using the Gompertz model
+#' mortality_event <- gompertz_event(
+#'   m_ind_traits_step = m_ind_traits_step,
+#'   a_coef_ukpds_ind_traits = a_coef_ukpds_ind_traits,
+#'   a_coef_ukpds_other_ind_traits = a_coef_ukpds_other_ind_traits,
+#'   health_outcome = health_outcome
+#' )
+#'
+#' # Print whether the mortality event occurred for each individual
+#' print(head(mortality_event))
+#' }
+gompertz_event <- function(
+    m_ind_traits_step,
+    a_coef_ukpds_ind_traits,
+    a_coef_ukpds_other_ind_traits,
+    health_outcome) {
   
   # Calculate patient-specific factors using model coefficients and patient data
-  patient_factors <- (a_ind_traits[, ,time_step] %*%  a_coef_ukpds_ind_traits[, health_outcome, 1] + 
-                        as.vector(a_coef_ukpds_other_ind_traits["lambda", health_outcome, 1]) )
+  patient_factors <- m_ind_traits_step %*%
+    a_coef_ukpds_ind_traits[, health_outcome, 1] +
+    as.vector(a_coef_ukpds_other_ind_traits["lambda", health_outcome, 1])
   
   # Compute cumulative hazard at the current time step
-  cum_hazard_t <- (1/a_coef_ukpds_other_ind_traits["rho", health_outcome, 1])* exp(patient_factors) * (exp(a_ind_traits[, "age" , time_step]*(a_coef_ukpds_other_ind_traits["rho", health_outcome, 1])) -1 )
-  # Compute cumulative hazard at the next time step (by adding 1 year to diabetes duration)
-  cum_hazard_t1 <- (1/a_coef_ukpds_other_ind_traits["rho", health_outcome, 1])* exp(patient_factors) * (exp((a_ind_traits[ , "age" , time_step]+1)*(a_coef_ukpds_other_ind_traits["rho", health_outcome, 1])) -1 )
+  cum_hazard_t <- (1/a_coef_ukpds_other_ind_traits["rho", health_outcome, 1]) *
+    exp(patient_factors) * (
+      exp(m_ind_traits_step[, "age"] *
+            (a_coef_ukpds_other_ind_traits["rho", health_outcome, 1])) -
+        1)
+  
+  # Compute cumulative hazard at the next time step (by adding 1 year to
+  # diabetes duration)
+  cum_hazard_t1 <- (1/a_coef_ukpds_other_ind_traits["rho", health_outcome, 1]) *
+    exp(patient_factors) * (
+      exp((m_ind_traits_step[, "age"] + 1) *
+            (a_coef_ukpds_other_ind_traits["rho", health_outcome, 1])) -
+        1)
   
   # Calculate transition probability
   trans_prob <- 1 - exp(cum_hazard_t - cum_hazard_t1)
   
   # Simulate whether the event occurs by comparing with a random uniform value
-  event <- trans_prob > runif(1)
+  event <- trans_prob > runif(nrow(m_ind_traits_step))
   
   # Return the updated matrix
   return(event)
 }
-
 
 #' @title Calculate Mortality Events for a Given Time Step
 #' 
